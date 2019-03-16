3d7985717cce1784534760205e10b7615a65171d
kellyhodis <kelly.hodis@protonmail.com>
2019-03-16 13:42:27 -0700

    Reverted changes back

--- holberton.h
+++ holberton.h
@@ -1,10 +1,14 @@
-#ifndef _PRINTF_H_
-#define _PRINTF_H_
-
-#include <stdio.h>
+#include <stdlib.h>		 
 #include <stdlib.h>
+#include <stdarg.h>		 
 #include <stdarg.h>
 
+typedef struct op		
+{		
+	char *c;		
+	void (*f)(va_list valist);		
+} op_t;		
 
+void _printf(const char * const print, ...);		
 
 #endif
