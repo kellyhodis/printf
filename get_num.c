5e5af3084a319dfdbf6290e8e959214eff8eec85
Kelly Hodis <668@holbertonschool.com>
2019-03-17 18:06:35 +0000

    Added comments for documentation

--- get_num.c
+++ get_num.c
@@ -1,10 +1,17 @@
 #include "holberton.h"
-
+/**
+* get_num - gets argument from valist and creates an int
+* @valist: argument from _printf variadic function
+*
+* Return: nothing
+*/
 void get_num(va_list valist)
 {
 	int n;
 
+	/* initializes n as an argument from valist */
 	n = va_arg(valist, int);
 
+	/* calls function to print the number */
 	print_int(n);
 }
