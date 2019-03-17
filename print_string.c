ff496e16ae664a79ded490d8e186b0c86f402fbd
Kelly Hodis <668@holbertonschool.com>
2019-03-17 18:37:16 +0000

    First commit

--- print_string.c
+++ print_string.c
@@ -1,9 +1,16 @@
 #include "holberton.h"
 
+/**
+* print_string - prints a string
+*
+* @valist: argument from _printf
+*/
+
 void print_string(va_list valist)
 {
 	int i;
 	char *s;
+
 	s = va_arg(valist, char*);
 
 	for (i = 0; s[i] != '\0'; i++)
