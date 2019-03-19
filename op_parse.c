b961e7c919de6590301249dec5b4f2f97484fa39
mapatelian <40901551+mapatelian@users.noreply.github.com>
2019-03-19 15:27:40 -0700

    Create op_parse.c

---
+++ op_parse.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+/**
+* op_parse - parses op_t and calls function pointer
+* @format: points to location of format at current count
+* @i: current index of format
+*
+* Return: count of characters
+*/
+int op_parse(const char *format, int *i, va_list valist)
+{
+                op_t array[] = {
+                {'s', print_string},
+                {'c', print_char},
+                {'i', get_num},
+                {'d', get_num},
+                {'u', get_num_u},
+                {'\0', NULL}
+        };
+        int j, n = 0;
+
+
+        for (j = 0; array[j].c != '\0'; j++)
+        {
+                if (format[*i + 1] == array[j].c)
+                {
+                        n += array[j].f(valist);
+                        *i = *i + 1;
+                        break;
+                }
+        }
+        if (array[j].c == '\0')
+                n += _putchar(format[*i]);
+
+
+        return (n);
+}
