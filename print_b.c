6436e8653408adce59bad458d0e39e0bba1b9102
mapatelian <40901551+mapatelian@users.noreply.github.com>
2019-03-19 23:31:02 -0700

    function that prints binary

---
+++ print_b.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_b - prints numbers
+ *
+ * @n: number to be printed
+ *
+ * Return: number of digits
+ */
+
+void print_b(int n)
+{
+
+	if (n / 10)
+		print_int(n / 10);
+	/* prints number */
+
+	_putchar((n % 10) + '0');
+}
